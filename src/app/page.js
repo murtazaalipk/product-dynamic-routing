bf3e32d9c6157d0a45f066c87be55db73f663243
Syed Murtaza Ali <125219980+murtazaalipk@users.noreply.github.com>
2025-06-27 10:42:30 +0500

    fetch data from api by asyncThunk

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,50 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { fetchProducts } from '@/redux/productSlice';
 
 
+// Utility function to slugify product titles
 function slugify(text) {
   return text.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
 }
 
 export default function HomePage() {
-  const products = useSelector((state) => state.products);
 
-
-//  const [products, setProducts] = useState([]);
-  const [search, setSearch] = useState('');
-  const [category, setCategory] = useState('');
+  // get products from redux store
+  // const products = useSelector((state) => state.products);
+  // const [search, setSearch] = useState('');
+  // const [category, setCategory] = useState('');
 
   
+  //get products from dummyjson api
+    const dispatch = useDispatch();
+    const [search, setSearch] = useState('');
+    const [category, setCategory] = useState('');
+    const { items, loading, error } = useSelector((state) => state.products)
 
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [dispatch])
+   if (loading) return <p>Loading...</p>;
+   if (error) return <p>Error: {error}</p>;
+
+
+  // fetch products from dummyjson api
+  // const [products, setProducts] = useState([]);
+  // const [search, setSearch] = useState('');
+  // const [category, setCategory] = useState('');
   // useEffect(() => {
   //   fetch('https://dummyjson.com/products')
   //     .then(res => res.json())
   //     .then(data => setProducts(data.products));
   // }, []);
 
-  const categories = [...new Set(products.map(p => p.category))];
+  const categories = [...new Set(items.map(p => p.category))];
 
-  const filtered = products.filter(p =>
+  const filtered = items.filter(p =>
     p.title.toLowerCase().includes(search.toLowerCase()) &&
     (category ? p.category === category : true)
   );
--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -1,15 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk  } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
+
+export const fetchProducts = createAsyncThunk(
+  'product/fetchProducts',
+  async () => {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    console.log(data.products);
+    return data.products;
+  }
+)
 
 const productSlice = createSlice({
   name: 'products',
-  initialState: [],
+  initialState: {
+    items: [],
+    loading: false,
+    error: null,
+  },
   reducers: {
-    addProduct: (state, action) => {
-      state.push({ id: uuidv4(), ...action.payload });
-    },
+    // addProduct: (state, action) => {
+    //   state.push({ id: uuidv4(), ...action.payload });
+    // },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      state.loading = false;
+      state.items = action.payload;
+    })
   },
 });
 
-export const { addProduct } = productSlice.actions;
+// export const { addProduct } = productSlice.actions;
 export default productSlice.reducer;
